2dbc0675f590344588d44d9bb0b83abef40e6c3e
Steve Grard <grard.steve@gmail.com>
2023-07-26 16:46:59 +0200

    Lot of work

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Test')
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'Cpa,cfsf.3217',
+    port = '3306',
+    database = 'api'
+)
+
+mycursor = mydb.cursor()
+mycursor.execute('SELECT * FROM posts')
+posts = mycursor.fetchall()
+for post in posts :
+    print('Title : ' + post[1])
+    print('Body : ' + post[2])
+    print('Author : ' + post[3])
+    print('Created date : ' + post[4].strftime('%d/%m/%Y'))
+    print('Updated date : ' + post[5].strftime('%d/%m/%Y'))
